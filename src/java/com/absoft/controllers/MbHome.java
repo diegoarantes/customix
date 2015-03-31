6304fc84ab488ffac8f7d5be1f39f986e7def2a2
Diego <Diego@DIEGO-CPD>
2015-03-31 17:23:52 -0300

    Faturamento Hoje

--- src/java/com/absoft/controllers/MbHome.java
+++ src/java/com/absoft/controllers/MbHome.java
@@ -1,9 +1,14 @@
 package com.absoft.controllers;
 
 import com.absoft.dao.DAOGenerico;
+import com.absoft.entities.Empresa;
+import com.absoft.entities.Pedido;
 import com.absoft.entities.Produto;
 import com.absoft.util.Mensagem;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
@@ -33,6 +38,19 @@ public class MbHome {
 
     public void setEstoqueBaixo(List<Produto> estoqueBaixo) {
         this.estoqueBaixo = estoqueBaixo;
+    }
+
+    public BigDecimal faturamentoHoje(Empresa emp) {
+        String hoje = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
+        List<Pedido> listPed = dao.listaCondicao(Pedido.class, "dataPedido = '" + hoje + "' AND empresa.id = " + emp.getId());
+        BigDecimal faturamento = BigDecimal.ZERO;
+
+        for (Pedido pedido : listPed) {
+            faturamento = faturamento.add(pedido.getTotal());
+        }
+        return faturamento;
+
     }
 
 }
